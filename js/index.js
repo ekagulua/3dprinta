faf7f40a9488edcfee6cdf226e9af54b78a978dd
Ека <1211eka@gmail.com>
2018-11-07 15:04:02 +0000

    Created initial files

--- js/index.js
+++ js/index.js
@@ -1,14 +1,2 @@
 document.querySelector('.page-loaded').innerText = (new Date()).toLocaleTimeString();
 
-document.querySelector('.load-html-ajax').addEventListener('klick', loadHtmlAjax);
-
-function loadHtmlAjax() {
-    const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if(xhr.readyState === 4 && xhr.status === 200){
-            document.querySelector('.html-placeholder').innerHTML = xhr.responseText;
-        } 
-    }
-    xhr.open('GET', 'our-work.html', true);
-    xhr.send();
-}
